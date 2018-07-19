bc16a4de4cc90c98ca4e8da13299a4283a37c023
Alejandro Criolani <alejandrodavidcriolani@gmail.com>
2018-07-19 15:21:57 -0300

    add horizontal tabs

--- tema1.js
+++ tema1.js
@@ -4,10 +4,15 @@ function init(){
     initAccordions();
     initPanels();
     showSlides(slideIndex);
+    initHorizontalTabs();
 }
 
 function initAccordions(){
     var acc = document.getElementsByClassName("accordion");
+
+    if(acc === null){
+        return;
+    }
 
     for(i = 0; i < acc.length; i++){
         acc[i].addEventListener("click", function(){
@@ -26,6 +31,10 @@ function initAccordions(){
 
 function initPanels(){
     var panels = document.getElementsByClassName("panel-header");
+
+    if(panels === null){
+        return;
+    }
     
     for(i = 0; i < panels.length; i++){
         panels[i].addEventListener("click", function(){
@@ -61,6 +70,11 @@ function showSlides(n){
     var i;
     var slides = document.getElementsByClassName("slides");
     var dots = document.getElementsByClassName("dot");
+
+    if(slides.length === 0 || dots.length === 0){
+        return;
+    }
+
     if(n > slides.length) {slideIndex = 1;}
     if(n < 1) {slideIndex = slides.length;}
     for(i = 0; i < slides.length; i++) {
@@ -71,6 +85,42 @@ function showSlides(n){
     }
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " dot-active";
+}
+
+function initHorizontalTabs(){
+    var i;
+    var horizontalTabs = document.getElementsByClassName("hor-tablinks");
+    var horTabcontents = document.getElementsByClassName("hor-tabcontent");
+
+    //horizontalTabs[horizontalTabs.length - 1].style.borderRight = "solid 1px #ccc";
+
+    console.log("Horizontal tabs: " + horizontalTabs.length);
+    console.log("Tab content: " + horTabcontents.length);
+
+    for(i = 0; i < horizontalTabs.length; i++){
+        horizontalTabs[i].addEventListener("click", displayTabContent.bind(this, i, 
+            horTabcontents, horizontalTabs));
+    }
+
+    if(horizontalTabs.length > 0 && horTabcontents.length > 0){
+        horTabcontents[0].style.display = "block";
+        horizontalTabs[0].className = "hor-tablinks tab-active";
+    }
+}
+
+function displayTabContent(index, horTabcontents, horizontalTabs){
+    var i = 0;
+    for(i = 0; i < horTabcontents.length; i++){
+        tabContent = horTabcontents[i];
+        tab = horizontalTabs[i];
+        if(i === index){
+            tabContent.style.display = "block";
+            tab.className = "hor-tablinks tab-active";
+        } else {
+            tabContent.style.display = "none";
+            tab.className = "hor-tablinks";
+        }
+    }
 }
 
 function topnavMenu(){
